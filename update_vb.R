66fa80ea7a3487c9ffe9c2097a2dee6e636a0204
Melody Hong <108764196+mhhong40@users.noreply.github.com>
2024-11-06 21:17:23 -0500

    Update update_vb.R

--- update_vb.R
+++ update_vb.R
@@ -3,27 +3,24 @@
 
 update_m2_theta_vb_ <- function(mu_theta_vb, sig2_theta_vb) {
 
-  sweep(mu_theta_vb^2, 2, sig2_theta_vb, "+")
+  mu_theta_vb^2 + sig2_theta_vb
 }
 
-update_sig2_theta_vb_ <- function(n, tau_vb, sig2_vb, c = 1) {
+update_sig2_theta_vb_ <- function(n, tau_vb, sig2_vb, c = 1) { # should be matrix!
 
-  # n - 1 instead of X_sq_norm
-  1 / (c * (sig2_vb + (n - 1)*tau_vb))
+  1 / (c * sweep(sig2_vb, 2, (n - 1)*tau_vb, "+"))
 
 }
 
 ## tau's updates
 update_eta_vb_ <- function(n, eta, c = 1) c * (n/2 + eta - 1)
 
-update_kappa_vb_ <- function(z, kappa, mat_x_m1, theta_vb, m2_theta, c = 1) {
+update_kappa_vb_ <- function(z, kappa, mat_x_m1, theta_vb, c = 1) {
 
   n <- nrow(z)
 
   c * (kappa + ( colSums(z^2) - 2 * colSums(z * mat_x_m1)  +
-                  colSums(mat_x_m1^2) - (n - 1) * colSums(theta_vb^2) )/ 2)
-  # for some reason, theta_vb is highly inflated -> kappa becomes negative ??
-
+                   colSums(mat_x_m1^2) + (n - 1) * colSums(theta_vb^2) )/ 2)
 }
 
 update_log_tau_vb_ <- function(eta_vb, kappa_vb) digamma(eta_vb) - log(kappa_vb)
@@ -31,7 +28,5 @@ update_log_tau_vb_ <- function(eta_vb, kappa_vb) digamma(eta_vb) - log(kappa_vb)
 ## lambda's update
 update_lambda_vb_ <- function(lambda, m2_theta, c = 1) {
 
-  c * (-1/2 * colSums(m2_theta) + lambda)
+  c * (1/2 * rowSums(m2_theta) + lambda)
 }
-
-
