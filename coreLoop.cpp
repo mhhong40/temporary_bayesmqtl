1fd0f675ce1e97ab0f85421ca982794200368dbb
Melody Hong <108764196+mhhong40@users.noreply.github.com>
2024-11-07 12:54:36 -0500

    Update coreLoop.cpp

--- coreLoop.cpp
+++ coreLoop.cpp
@@ -1,4 +1,5 @@
 #include "utils.h"
+// Not used in practice as there's a bug here that causes RStudio to crash... fix/find out ASAP
 
 // Parameter theta_st is the most computationally expensive to update, so its update is coded here.
 // Hélène -- this is the code you wrote for locus's coreLoop(). I directly adapted it here to save time, sorry.
@@ -9,7 +10,7 @@ void coreLoop(const MapMat z,
               MapMat theta_vb,
               MapArr2D mu_theta_vb,
               const MapArr1D tau_vb,
-              const MapArr1D sig2_theta_vb,
+              const MapArr2D sig2_theta_vb,
               const double c = 1) {
 
   for (int s = 0; s < X.cols(); ++s) {
@@ -17,7 +18,7 @@ void coreLoop(const MapMat z,
    // For some reason, this code runs very slowly -- what's the performance bottleneck?
    mat_x_m1.noalias() -= X.col(s) * theta_vb.row(s); // take away the contribution of the sth predictor
 
-   mu_theta_vb.row(s) = c * sig2_theta_vb * tau_vb *
+   mu_theta_vb.row(s) = c * sig2_theta_vb.row(s) * tau_vb *
      ((z - mat_x_m1).transpose() * X.col(s)).array();
 
    theta_vb.row(s) = mu_theta_vb.row(s);
